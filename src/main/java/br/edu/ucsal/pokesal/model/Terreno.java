fe2c882ff3983c043556661a6abd48860dd550a0
netomrs1 <netoarivaldo9@gmail.com>
2026-09-15 23:38:06 -0300

    fix: atende apontamentos do code review sobre construtor e ux

--- src/main/java/br/edu/ucsal/pokesal/model/Terreno.java
+++ src/main/java/br/edu/ucsal/pokesal/model/Terreno.java
@@ -4,7 +4,7 @@ package br.edu.ucsal.pokesal.model;
  * Define os terrenos fixos.
  */
 public enum Terreno {
-	ASFALTO_QUENTE("Asfalto Quente"), POCA_CHUVA("Poça Chuva"), CANTEIRO_CENTRAL("Canteiro Central");
+	ASFALTO_QUENTE("Asfalto Quente"), POCA_CHUVA("Poça de Chuva"), CANTEIRO_CENTRAL("Canteiro Central");
 	private final String nomeFormatado;
 
 	private Terreno(String nomeFormatado) {
